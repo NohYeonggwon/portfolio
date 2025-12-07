b7abc14c06bfcd0b860acbb92f026cf1657fd9e4
Noh_yg <skyt852@gmail.com>
2025-12-08 02:56:48 +0900

    이미지 아래 a태그 수정

--- src/component/about/me/right/Right.tsx
+++ src/component/about/me/right/Right.tsx
@@ -7,8 +7,12 @@ export default function Right() {
       <div className="flex-none flex flex-col gap-6 items-center">
         <img className="w-75 h-75 rounded-full" src={myPhoto} alt="me" />
         <a href="" download className="flex">
-          <div className="flex items-center gap-2 px-3 py-2 bg-gray-200 rounded-lg text-gray-500 hover:bg-gray-500 hover:text-white">
-            <a href="/resume.pdf" download>
+          <div className=" bg-gray-200 rounded-lg text-gray-500 hover:bg-gray-500 hover:text-white">
+            <a
+              className="flex items-center gap-2 px-3 py-2"
+              href="/resume.pdf"
+              download
+            >
               <span className="text-sm">이력서 다운로드</span> <Download />
             </a>
           </div>
