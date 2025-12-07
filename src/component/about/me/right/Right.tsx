5baa158d5b78556b8267fb21c0f031b543411eff
Noh_yg <skyt852@gmail.com>
2025-12-08 02:53:30 +0900

    이미지 추가

--- src/component/about/me/right/Right.tsx
+++ src/component/about/me/right/Right.tsx
@@ -8,7 +8,9 @@ export default function Right() {
         <img className="w-75 h-75 rounded-full" src={myPhoto} alt="me" />
         <a href="" download className="flex">
           <div className="flex items-center gap-2 px-3 py-2 bg-gray-200 rounded-lg text-gray-500 hover:bg-gray-500 hover:text-white">
-            <span className="text-sm">이력서 다운로드</span> <Download />
+            <a href="/resume.pdf" download>
+              <span className="text-sm">이력서 다운로드</span> <Download />
+            </a>
           </div>
         </a>
       </div>
